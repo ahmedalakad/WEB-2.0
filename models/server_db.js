b2643a73f4c5df2a8fb7b803c5778e01ec69bbfd
ahmedalakad@live.com <hyperledger@SherifKhairy.ebi.local>
2018-12-06 17:55:23 +0200

    getting data back

--- models/server_db.js
+++ models/server_db.js
@@ -3,9 +3,19 @@ var db = require('../db.js');
 save_user_information = (data) => new Promise((resolve,reject)=>{
   db.query('INSERT INTO lottery_information SET ?',data, function(err,results, fields){
     if(err){
-      reject('could not insert into lottery information');
+      reject('could not insert into lottery_information');
     }
     resolve('Succesful');
+  });
+})
+
+
+get_total_amount = (data) => new Promise((resolve,reject)=>{
+  db.query('select sum(amount) as total_amount from lottery_information ',null, function(err,results, fields){
+    if(err){
+      reject('could not get total amount');
+    }
+    resolve(results);
   });
 })
 
